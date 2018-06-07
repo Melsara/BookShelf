a1e1d1ebfbaedcbd51d129a01a6005b339740738
Melsara <kaxhortz@gmail.com>
2018-06-07 22:40:30 +0200

    Creating SQLite data helpers.

--- app/src/main/java/eu/escapeadvisor/bookshelf/data/BookshelfContract.java
+++ app/src/main/java/eu/escapeadvisor/bookshelf/data/BookshelfContract.java
@@ -1,4 +1,20 @@
 package eu.escapeadvisor.bookshelf.data;
 
-public class BookshelfContract {
+import android.provider.BaseColumns;
+
+public final class BookshelfContract {
+
+    private BookshelfContract () {
+
+    }
+
+    public static final class BookshelfEntry implements BaseColumns {
+        public static final String TABLE_NAME = "books";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_BOOKS_PRODUCTNAME = "product_name";
+        public static final String COLUMN_BOOKS_PRICE = "price";
+        public static final String COLUMN_BOOKS_QUANTITY = "quantity";
+        public static final String COLUMN_BOOKS_SUPPLIERNAME = "supplier_name";
+        public static final String COLUMN_BOOKS_SUPPLIERPHONENUMBER = "supplier_phonenumber";
+    }
 }
--- app/src/main/java/eu/escapeadvisor/bookshelf/data/BookshelfDbHelper.java
+++ app/src/main/java/eu/escapeadvisor/bookshelf/data/BookshelfDbHelper.java
@@ -1,4 +1,38 @@
 package eu.escapeadvisor.bookshelf.data;
 
-public class BookshelfDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import eu.escapeadvisor.bookshelf.data.BookshelfContract.BookshelfEntry;
+
+public class BookshelfDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Bookshelf.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_BOOKS_TABLE =
+            "CREATE TABLE " + BookshelfEntry.TABLE_NAME + " (" +
+                    BookshelfEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    BookshelfEntry.COLUMN_BOOKS_PRODUCTNAME + " TEXT NOT NULL," +
+                    BookshelfEntry.COLUMN_BOOKS_PRICE + " REAL default 0.0," +
+                    BookshelfEntry.COLUMN_BOOKS_QUANTITY + " INTEGER," +
+                    BookshelfEntry.COLUMN_BOOKS_SUPPLIERNAME + "TEXT," +
+                    BookshelfEntry.COLUMN_BOOKS_SUPPLIERPHONENUMBER + "TEXT)";
+
+    private static final String SQL_DELETE_BOOKS_TABLE =
+            "DROP TABLE IF EXISTS " + BookshelfEntry.TABLE_NAME;
+
+    public BookshelfDbHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(SQL_DELETE_BOOKS_TABLE);
+        onCreate(db);
+    }
 }
